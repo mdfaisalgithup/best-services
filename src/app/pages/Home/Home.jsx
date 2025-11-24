e1264bebdb854da7a8ff858d012f94f214add3d0
unknown <mdfaisald040@gmail.com>
2025-11-24 21:06:10 +0600

    'u'

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -76,8 +76,59 @@ export default function Home() {
 
 </div>
 
+{/* section */}
+
+<div>
+<div className="xl:mx-[240px] lg:mx-[150px] md:mx-[100px] sm:mx-[10px] space-y-5 my-2">
+  <div className="text-center">
+  <h2 className="font-bold text-[24px] text-[#3A3A3A] ">Our Mission</h2>
+<div className="mt-4">
+   <h3 className="font-semibold text-[#3A3A3A] text-[19px]">Ma Construction Maintenance LTD.</h3>
+ <p className="text-[#747474]">is proud to represent the essence of dedication and professionalism in the construction industry. Our main goal is to maintain high standards in every project, with workers equipped with appropriate safety equipment, and to complete the work with precision and care.</p>
+</div>
+</div>
+
+</div>
 
 
+<div className="relative w-full h-48 bg-fixed bg-center bg-cover" 
+     style={{ backgroundImage: "url('https://i.ibb.co.com/gZkwxZH4/savannah-du-toit-h3-FRYkgq-JSg-unsplash.jpg')" }}>
+  
+  {/* Dark overlay */}
+  <div className="absolute inset-0 bg-[#02020275] bg-opacity-60"></div>
+  
+  {/* Centered content */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+    <h1 className="text-2xl md:text-4xl font-bold mb-4">Lets Make Your Dream House With Our ProfessionalsYour Title Here</h1>
+    <p className="mb-6">Some subtitle or description goes here.</p>
+    <button className="bg-transparent border-[1px] border-[#7575758c] text-white font-semibold py-2 px-6 rounded">
+      Click Me
+    </button>
+  </div>
+</div>
+
+
+
+
+
+<div>
+
+  
+  <h2 className="font-bold text-[20px] text-center text-black">Our Services</h2>
+
+<div>
+  <h2>
+Building and Construction </h2>
+<p></p>
+</div>
+
+</div>
+
+
+</div>
+
+
+{/* section */}
 <div className="xl:mx-[240px] lg:mx-[150px] md:mx-[100px] sm:mx-[10px] ">
 
 <h2 className="font-semibold mt-6 text-center text-black text-2xl">
@@ -90,7 +141,7 @@ export default function Home() {
     grid-cols-1 
     sm:grid-cols-2 
     md:grid-cols-3 
-    lg:grid-cols-4 
+    lg:grid-cols-3
     gap-6 
     p-5
   "
@@ -99,9 +150,9 @@ export default function Home() {
     <div
       key={item.id}
       className="
-        border 
-        border-gray-200 
-        rounded-xl 
+        border-[1px]
+        border-gray-300 
+        rounded-md
         shadow-md 
         overflow-hidden 
         hover:shadow-lg 
@@ -111,13 +162,19 @@ export default function Home() {
       <img
         src={item.thumbnail}
         alt={item.title}
-        className="w-full h-48 object-cover"
+        rounded-xl 
+        className="w-full  h-48 object-cover"
       />
 
       <div className="p-4">
         <h3 className="font-semibold text-lg">{item.title}</h3>
         <p className="text-gray-600 text-sm">{item.description}</p>
       </div>
+
+<div>
+  <button type="button">More</button>
+</div>
+
     </div>
   ))}
 </div>
