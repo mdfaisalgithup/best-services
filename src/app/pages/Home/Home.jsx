912c28921982cd0b863af7529401dd738df7352c
unknown <mdfaisald040@gmail.com>
2025-11-25 18:41:45 +0600

    U

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -196,12 +196,12 @@ Building and Construction </h2>
 </div>
 
 
-<div className="flex items-center my-2 justify-center gap-x-4 bg-[#027c02]">
-<div>
+<div className="xl:flex lg:flex md:flex items-center flex-wrap my-2 justify-center gap-x-4 bg-[#027c02]">
+<div className="py-2">
     <Image className="w-[90px] bg-white  h-[90px] rounded-full mx-auto my-2" src="https://i.ibb.co.com/0jLgLv1G/Chat-GPT-Image-Nov-25-2025-05-57-46-PM-removebg-preview.png" alt="logo" width={100} height={100}/>
 </div>
   <div>
-    <h2 className="font-bold text-[25px]">Govt Licensed Approved</h2>
+    <h2 className="font-bold xl:text-[25px] lg:text-[25px] md:text-[25px] text-[14px] text-center">Govt Licensed Approved</h2>
   </div>
 </div>
 
