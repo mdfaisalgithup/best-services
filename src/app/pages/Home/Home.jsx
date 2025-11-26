6600f0084d6e89967e5711ed8bbfbf0e28b44128
unknown <mdfaisald040@gmail.com>
2025-11-26 12:38:37 +0600

    U

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -11,44 +11,44 @@ import Typewriter from 'typewriter-effect';
 export default function Home() {
 
 
-  const dataJson = [
-    {
-      id: 1,
-      title: "Special Morning Moment",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "Captured during a beautiful morning."
-    },
-    {
-      id: 2,
-      title: "Perfect Smile",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "A sweet and joyful smiling moment."
-    },
-    {
-      id: 3,
-      title: "Nature Walk",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "Walking through nature with peace."
-    },
-    {
-      id: 4,
-      title: "Special Morning Moment",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "Captured during a beautiful morning."
-    },
-    {
-      id: 5,
-      title: "Perfect Smile",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "A sweet and joyful smiling moment."
-    },
-    {
-      id: 6,
-      title: "Nature Walk",
-      thumbnail: "https://i.ibb.co.com/bMWfpJ5n/Whats-App-Image-2025-11-23-at-8-46-46-AM.jpg",
-      description: "Walking through nature with peace."
-    }
-  ];
+const dataJson = [
+  {
+    id: 1,
+    title: "Interior Plastering",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "Smooth interior plastering for clean, strong, and paint-ready walls and ceilings."
+  },
+  {
+    id: 2,
+    title: "Exterior Plastering",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "Durable exterior plastering that protects your home from weather and damage."
+  },
+  {
+    id: 3,
+    title: "Skim Coating",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "A thin finishing layer to make walls and ceilings smooth and flawless."
+  },
+  {
+    id: 4,
+    title: "Gib Stopping",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "Professional Gib stopping for seamless joints and perfectly even surfaces."
+  },
+  {
+    id: 5,
+    title: "Crack Repairs",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "Repairing cracks in walls and ceilings to restore strength and appearance."
+  },
+  {
+    id: 6,
+    title: "Interior & Exterior Painting",
+    thumbnail: "https://i.ibb.co.com/vb3Pb66/1-premium-photo-1681589433879-c823909b13be.jpg",
+    description: "High-quality painting services that enhance and protect your home."
+  }
+];
 
 
  
@@ -139,7 +139,7 @@ export default function Home() {
 
 
 
-
+{/* 
 <div>
 
   
@@ -151,58 +151,74 @@ Building and Construction </h2>
 <p></p>
 </div>
 
-</div>
+</div> */}
 
 
 </div>
 
 
 {/* section */}
-<div className="xl:mx-[240px] lg:mx-[150px] md:mx-[100px] sm:mx-[10px] ">
+<div className="xl:mx-[240px] lg:mx-[150px] md:mx-[100px] mx-[10px] ">
 
 <h2 className="font-semibold mt-6 text-center text-black text-2xl">
   Our Services
 </h2>
+<p className="text-black text-center font-medium text-orange-500 font-semibold italic">Plaster & Painting</p>
 
 <div
   className="
     grid 
     grid-cols-1 
-    sm:grid-cols-2 
+    sm:grid-cols-2  
     md:grid-cols-3 
     lg:grid-cols-3
     gap-6 
-    p-5
+    my-6
   "
 >
   {slides.map((item) => (
     <div
       key={item.id}
       className="
-        border-[1px]
-        border-gray-300 
-        rounded-md
-        shadow-md 
-        overflow-hidden 
-        hover:shadow-lg 
-        transition
+        rounded-md border-1 border-[#ddd] shadow-xl
+       
       "
     >
-      <img
+<div>
+        <img
         src={item.thumbnail}
         alt={item.title}
         rounded-xl 
-        className="w-full  h-48 object-cover"
+        className="w-full  h-48 object-cover rounded-t-md"
       />
+      <div className="flex flex-wrap justify-between items-center   ">
 
-      <div className="p-4">
-        <h3 className="font-semibold text-lg">{item.title}</h3>
-        <p className="text-gray-600 text-sm">{item.description}</p>
+ 
+
+  
+
+   <div className="p-4">
+        <h3 className="font-semibold text-lg text-black my-2">{item.title}</h3>
+    
+       <p className="text-black font-normal text-left text-[13px]">
+  {item?.description.length > 76 ? (
+    <>
+      {item?.description.slice(0, 70)}{" "}
+      <span className="text-green-600">More...</span>
+    </>
+  ) : (
+    item?.description
+  )}
+</p>
+
       </div>
+  </div>
 
-<div>
-  <button type="button">More</button>
-</div>
+<div className="flex justify-center my-2">
+  <button className="bg-amber-400 rounded-full hover:bg-orange-500 transition-all cursor-pointer shadow-2xl px-6 py-2" type="button">More</button>
+</div> 
+     </div>
+    
 
     </div>
   ))}
